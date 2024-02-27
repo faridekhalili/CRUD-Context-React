d2cf5bdd5cb533a07517af192371f8966958327e
Faridekhalili <khalili.f@northeastern.edu>
2024-02-27 14:22:37 -0500

    useRef instead of useState.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,35 +1,47 @@
 import { Styles } from "./styles";
 
 interface IProps {
-    name: string;
-    label?: string;
-    type?: string;
-    placeholder?: string;
-    onChangeText?(value: string): void;
-    autoFocus?: boolean;
-    value?: any;
+  name: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  onChangeText?(value: string): void;
+  onBlur?(): void;
+  autoFocus?: boolean;
+  value?: any;
+  defaultValue?: string | number;
 }
 
-export const Input = ({ type = 'text', name, label, placeholder, onChangeText, autoFocus = false, value }: IProps) => {
-    return (
-        <Styles.Container>
-            {
-                !!label && <Styles.Label htmlFor={type}>{label}</Styles.Label>
-            }
-            <Styles.Input
-                placeholder={placeholder ?? label}
-                type={type}
-                name={name}
-                id={type}
-                autoComplete='off'
-                autoFocus={autoFocus}
-                value={value}
-                {
-                ...(!!onChangeText && {
-                    onChange: (event) => onChangeText(event?.target?.value),
-                })
-                }
-            />
-        </Styles.Container>
-    )
-}
+export const Input = ({
+  type = "text",
+  name,
+  label,
+  placeholder,
+  onChangeText,
+  onBlur,
+  autoFocus = false,
+  value,
+  defaultValue,
+}: IProps) => {
+  return (
+    <Styles.Container>
+      {!!label && <Styles.Label htmlFor={type}>{label}</Styles.Label>}
+      <Styles.Input
+        placeholder={placeholder ?? label}
+        type={type}
+        name={name}
+        id={type}
+        autoComplete="off"
+        autoFocus={autoFocus}
+        defaultValue={defaultValue}
+        value={value}
+        {...(!!onChangeText && {
+          onChange: (event) => onChangeText(event?.target?.value),
+        })}
+        {...(onBlur && {
+          onBlur: () => onBlur(),
+        })}
+      />
+    </Styles.Container>
+  );
+};
--- src/components/item/form.tsx
+++ src/components/item/form.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from "react";
 import { Styles } from "./styles";
 import { IItem } from "../../interfaces/IItem";
 
@@ -8,50 +8,67 @@ import { Input } from "../input";
 import { SaveIcon } from "../icons/save";
 import { CloseIcon } from "../icons/close";
 
-import { useContextHook } from '../../context/hook';
+import { useContextHook } from "../../context/hook";
 
 export const ItemForm = ({ ...item }: IItem) => {
+  const { toggleIsEditing, editItem } = useContextHook();
 
-    const { toggleIsEditing, editItem } = useContextHook();
+  const [formState, setFormState] = useState(item);
+  const ageInputRef = useRef<string>((item.age ?? "").toString());
+  const whatsappInputRef = useRef<string>((item.whatsapp ?? "").toString());
+  const nicknameInputRef = useRef<string>(item.nickname ?? "");
 
-    const [formState, setFormState] = useState(item);
+  const { nickname = "", whatsapp = "", age = "", id } = formState || {};
 
-    const { nickname = '', whatsapp = '', age = '', id } = formState || {};
+  const updateAge = () => {
+    const ageValue = ageInputRef.current || "0";
+    setFormState((old) => ({ ...old, age: Number(ageValue) }));
+  };
 
-    return (
-        <>
-            <Input
-                name="nickname"
-                placeholder="Nickname"
-                onChangeText={(v) =>
-                    setFormState((old) => ({ ...old, nickname: v }))}
-                value={nickname}
-                autoFocus
-            />
-            <Input
-                name="age"
-                placeholder="Age"
-                type="number"
-                onChangeText={(v) =>
-                    setFormState((old) => ({ ...old, age: ~~v }))}
-                value={age}
-            />
-            <Input
-                name="whatsapp"
-                placeholder="Whatsapp number"
-                type="number"
-                onChangeText={(v) =>
-                    setFormState((old) => ({ ...old, whatsapp: ~~v }))}
-                value={whatsapp}
-            />
-            <Styles.GroupButtons>
-                <Button onClick={() => editItem(formState)}>
-                    <SaveIcon />
-                </Button>
-                <Button onClick={() => toggleIsEditing(id)}>
-                    <CloseIcon />
-                </Button>
-            </Styles.GroupButtons>
-        </>
-    )
-}
+  const updateWhatsapp = () => {
+    const whatsappValue = whatsappInputRef.current || "";
+    setFormState((old) => ({ ...old, whatsapp: Number(whatsappValue) }));
+  };
+
+  const updateNickname = () => {
+    const nicknameValue = nicknameInputRef.current || "";
+    setFormState((old) => ({ ...old, nickname: nicknameValue }));
+  };
+
+  return (
+    <>
+      <Input
+        name="nickname"
+        placeholder="Nickname"
+        onChangeText={(v) => (nicknameInputRef.current = v)}
+        onBlur={updateNickname}
+        defaultValue={nickname}
+        autoFocus
+      />
+      <Input
+        name="age"
+        placeholder="Age"
+        type="number"
+        onChangeText={(v) => (ageInputRef.current = v)}
+        onBlur={updateAge}
+        defaultValue={age}
+      />
+      <Input
+        name="whatsapp"
+        placeholder="Whatsapp number"
+        type="number"
+        onChangeText={(v) => (whatsappInputRef.current = v)}
+        onBlur={updateWhatsapp}
+        defaultValue={whatsapp}
+      />
+      <Styles.GroupButtons>
+        <Button onClick={() => editItem(formState)}>
+          <SaveIcon />
+        </Button>
+        <Button onClick={() => toggleIsEditing(id)}>
+          <CloseIcon />
+        </Button>
+      </Styles.GroupButtons>
+    </>
+  );
+};
