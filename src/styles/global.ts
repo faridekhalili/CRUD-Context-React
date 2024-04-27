ef2dd7bb3a78ddeeb7ebeca27acc3ce680f82c4d
Faridekhalili <khalili.f@northeastern.edu>
2024-04-26 20:06:42 -0400

    Memoize component.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
@@ -18,9 +19,9 @@ export const GlobalStyles = createGlobalStyle`
     }
 `;
 
-export const Container = styled.div.attrs({ className: "container" })`
+export const Container = React.memo(styled.div.attrs({ className: "container" })`
     width: 1200px;
     max-width: 100%;
     margin: 0 auto;
     padding: 0 20px;
-`;
+`);
